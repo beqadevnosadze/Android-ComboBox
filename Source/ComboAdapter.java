4bde9e7902ff8d1cb53bc1f23f5240b621b41bbb
beqadevnosadze <beqa_devnosadze@yahoo.com>
2016-02-10 23:28:22 +0400

    Drop Down Row Layout File

---
+++ Source/ComboAdapter.java
@@ -0,0 +1,238 @@
+package ge.ndc.ndc.controls;
+
+import android.content.Context;
+import android.graphics.Color;
+import android.text.Html;
+import android.text.Spannable;
+import android.text.SpannableStringBuilder;
+import android.text.Spanned;
+import android.text.TextUtils;
+import android.text.style.BackgroundColorSpan;
+import android.text.style.ForegroundColorSpan;
+import android.text.style.StyleSpan;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
+import android.widget.TextView;
+import android.widget.Filter;
+
+import java.lang.reflect.Field;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Objects;
+
+import ge.ndc.ndc.R;
+
+/**
+ * Created by beqa on 10.02.2016.
+ */
+public class ComboAdapter<T> extends ArrayAdapter<T> {
+
+    Context _context;
+    int _resource;
+    List<T> _items,_tempItems,_suggestions;
+    String _filterString="";
+
+    private String _DisplayMember;
+
+    public ComboAdapter(Context context, int resource,ArrayList<T> Items,String DisplayMember) {
+        super(context, resource,Items);
+        _context=context;
+        _resource=resource;
+        _items=Items;
+        _DisplayMember=DisplayMember;
+        _suggestions = new ArrayList<T>();
+        _tempItems=new ArrayList<T>(Items);//(ArrayList<T>)Items.clone();
+    }
+
+    final ForegroundColorSpan fcs = new ForegroundColorSpan(Color.rgb(0, 78, 238));
+    final StyleSpan bss = new StyleSpan(android.graphics.Typeface.BOLD);
+    @Override
+    public View getView(int position, View convertView, ViewGroup parent) {
+        View view = convertView;
+        if (convertView == null) {
+            LayoutInflater inflater = (LayoutInflater) _context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+            view = inflater.inflate(R.layout.row_combo, parent, false);
+        }
+        Object item = _items.get(position);
+        if (item != null) {
+            TextView lblName = (TextView) view.findViewById(R.id.lbl_name);
+            if (lblName != null){
+                String value="";
+                try {
+                    value=item.getClass().getField(_DisplayMember).get(item).toString();
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                } catch (NoSuchFieldException e) {
+                    e.printStackTrace();
+                }
+
+
+                final SpannableStringBuilder sb = new SpannableStringBuilder(value);
+                try {
+                    int index=TextUtils.indexOf(value.toLowerCase(),_filterString.toLowerCase() );
+                    if(index >= 0 && _filterString!=null && _filterString.length() > 0){
+                        int length=_filterString.length();
+
+                        sb.setSpan(fcs,index, index+length, Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+                        sb.setSpan(bss,index, index+length, Spannable.SPAN_INCLUSIVE_INCLUSIVE);
+
+
+                        lblName.setText(sb);
+//                    value = value.replaceAll(_filterString,"<font color='#004eee'><b>"+_filterString+"</b></font>");
+//                    lblName.setText(Html.fromHtml(value));
+                    }
+                    else {
+                        lblName.setText(value);
+                    }
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+
+
+//                value = value.replaceAll(_filterString,"<b>"+_filterString+"</b>");
+
+//                lblName.setText(Html.fromHtml(value));
+
+//
+//                sb.clear();
+            }
+        }
+        return view;
+    }
+
+    @Override
+    public Filter getFilter() {
+        return nameFilter;
+    }
+
+
+
+    /**
+     * Custom Filter implementation for custom suggestions we provide.
+     */
+    Filter nameFilter = new Filter() {
+        @Override
+        public CharSequence convertResultToString(Object resultValue) {
+            String value="";
+            try {
+                value=resultValue.getClass().getField(_DisplayMember).get(resultValue).toString();
+            } catch (IllegalAccessException e) {
+                e.printStackTrace();
+            } catch (NoSuchFieldException e) {
+                e.printStackTrace();
+            }
+            return value;
+        }
+
+        boolean FilterStarted=false;
+        @Override
+        protected FilterResults performFiltering(CharSequence constraint) {
+            if(FilterStarted){
+                return new FilterResults();
+            }
+            FilterResults filterResults = new FilterResults();
+            if (constraint != null) {
+                FilterStarted=true;
+                _suggestions.clear();
+
+                if(_tempItems.size() == 0){
+                    return new FilterResults();
+                }
+
+
+                if (constraint == null || constraint.length() == 0) {
+                    ArrayList<T> list;
+                    list = new ArrayList<T>(_tempItems);
+                    filterResults.values = list;
+                    filterResults.count = list.size();
+                    return filterResults;
+                }
+
+                Field field = null;
+                try {
+                   field=_tempItems.get(0).getClass().getField(_DisplayMember);
+                }  catch (NoSuchFieldException e) {
+                    e.printStackTrace();
+                    return new FilterResults();
+                }
+
+                String prefixString = constraint.toString().toLowerCase();
+                final int count = _tempItems.size();
+                for (int i = 0; i < count; i++) {
+                    final T item = _tempItems.get(i);
+                    String valueText="";
+                    try {
+                        valueText=field.get(item).toString().toLowerCase();
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    }
+
+
+                    if (valueText.startsWith(prefixString)) {
+                        _suggestions.add(item);
+                    } else {
+                        final String[] words = valueText.split(" ");
+                        final int wordCount = words.length;
+
+                        // Start at index 0, in case valueText starts with space(s)
+                        for (int k = 0; k < wordCount; k++) {
+                            if (words[k].startsWith(prefixString)) {
+                                _suggestions.add(item);
+                                break;
+                            }
+                        }
+                    }
+
+                }
+
+
+                filterResults.values = _suggestions;
+                filterResults.count =_suggestions.size();
+
+                FilterStarted=false;
+                return filterResults;
+            } else {
+                filterResults.values = _tempItems;
+                filterResults.count =_tempItems.size();
+                _filterString="";
+                return filterResults;
+            }
+        }
+
+        private boolean publishStarted=false;
+        @Override
+        protected void publishResults(CharSequence constraint, FilterResults results) {
+            if(publishStarted) {
+                return;
+            }
+
+            if(constraint != null){
+                _filterString=constraint.toString();
+            }
+            else {
+                _filterString="";
+            }
+
+            if (results != null && results.count > 0) {
+
+                List<T> filterList = (ArrayList<T>) results.values;
+                publishStarted=true;
+                clear();
+                final int count = filterList.size();
+                for (int i = 0; i < count; i++) {
+                    if(i>filterList.size()) break;
+                    T item=filterList.get(i);
+                    add(item);
+                }
+                notifyDataSetChanged();
+
+                publishStarted=false;
+            } else {
+                notifyDataSetInvalidated();
+            }
+        }
+    };
+}
---
+++ Source/ComboBox.java
@@ -0,0 +1,174 @@
+package ge.ndc.ndc.controls;
+
+import android.app.Activity;
+import android.content.Context;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.util.AttributeSet;
+import android.view.KeyEvent;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AutoCompleteTextView;
+import java.util.ArrayList;
+
+/**
+ * Created by beqa on 10.02.2016.
+ */
+public class ComboBox extends AutoCompleteTextView{
+
+    private ArrayList<?> _dataSource;
+    private Object _selectedValue;
+    private Object _selectedItem;
+    private String _selectedText;
+    private String _displayMember;
+    private String _valueMember;
+
+
+    public ComboBox(Context context, AttributeSet attrs){
+        super(context,attrs);
+        SetupComboBox();
+    }
+    public ComboBox(Context context, AttributeSet attrs, int defStyle){
+        super(context,attrs,defStyle);
+        SetupComboBox();
+    }
+    public ComboBox(Context context) {
+        super(context);
+        SetupComboBox();
+    }
+
+
+    private void SetupComboBox(){
+        this.setThreshold(1);
+        final AutoCompleteTextView  textView=this;
+
+        this.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                textView.showDropDown();
+            }
+        });
+
+        //Set Onclick Listener When Item Clicked
+        this.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            public void onItemClick(AdapterView<?> arg0, View arg1, int arg2, long arg3) {
+                Object item=arg0.getItemAtPosition(arg2);
+                if(item != null){
+                    Object val= null;
+                    try {
+                        val = item.getClass().getField(_valueMember).get(item);
+                        Object text=item.getClass().getField(_displayMember).get(item);
+                        _selectedValue=val;
+                        _selectedText=text.toString();
+                        _selectedItem=item;
+                    } catch (IllegalAccessException e) {
+                        e.printStackTrace();
+                    } catch (NoSuchFieldException e) {
+                        e.printStackTrace();
+                    }
+                }
+            }
+        });
+
+        this.setOnKeyListener(new OnKeyListener() {
+            @Override
+            public boolean onKey(View arg0, int arg1, KeyEvent arg2) {
+                _selectedValue=null;
+                _selectedText="";
+                _selectedItem=null;
+                return false;
+            }
+        });
+
+
+
+        this.addTextChangedListener(new TextWatcher() {
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+            }
+
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+
+            }
+        });
+    }
+
+    public void SetDisplayMember( String value ){
+        _displayMember=value;
+}
+
+    public void SetValueMember( String value ){
+        _valueMember=value;
+    }
+
+    public Object GetSelectedItem(){
+        return _selectedItem;
+    }
+
+    public Object GetSelectedText(){
+        return _selectedText;
+    }
+
+    public Object GetSelectedValue(){
+        return _selectedValue;
+    }
+
+    public void SetSelectedValue(Object value) {
+        if(value==null){
+            setText("");
+            _selectedValue=value;
+            _selectedText="";
+            _selectedItem=value;
+            return;
+        }
+        if(_dataSource != null && _valueMember != null){
+            for (Object v:_dataSource){
+                try {
+                    Object val=v.getClass().getField(_valueMember).get(v);
+                    if(val.equals(value)){
+                        Object text=v.getClass().getField(_displayMember).get(v);
+                        setText(text.toString());
+                        _selectedValue=val;
+                        _selectedText=text.toString();
+                        _selectedItem=v;
+                    }
+                } catch (IllegalAccessException e) {
+                    e.printStackTrace();
+                } catch (NoSuchFieldException e) {
+                    e.printStackTrace();
+                }
+            }
+        }
+    }
+
+    public Object GetDataSource(){
+        return _dataSource;
+    }
+
+    public <T> void SetDataSource(ArrayList<T> source,Activity context,String DisplayMember,String ValueMember){
+
+        _displayMember=DisplayMember;
+        _valueMember=ValueMember;
+
+        SetDataSource(source,  context);
+    }
+
+   public <T> void SetDataSource(ArrayList<T> source,Activity context){
+
+       _dataSource=(ArrayList<T>)source.clone();
+
+       ComboAdapter comboAdapter=new ComboAdapter(context,android.R.layout.simple_dropdown_item_1line,source,_displayMember);
+
+       setAdapter(comboAdapter);
+   }
+
+
+}
